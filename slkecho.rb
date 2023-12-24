23db3c4b7f1e5a16c85c7650fbc0137e551c25e4
okonomi <okonomi@oknm.jp>
2023-12-24 18:09:18 +0900

    initial

---
+++ slkecho.rb
@@ -0,0 +1,54 @@
+require 'net/http'
+require 'uri'
+require 'json'
+require 'optparse'
+
+# コマンドライン引数を解析する
+options = {}
+opts = OptionParser.new do |opts|
+  opts.banner = "Usage: echo2slack.rb [options] or echo | echo2slack.rb [options]"
+
+  opts.on("-c", "--channel CHANNEL", "Slack channel to post the message") do |c|
+    options[:channel] = c
+  end
+end
+opts.parse!
+
+# メッセージを標準入力から取得
+options[:message] = ARGV.empty? ? STDIN.read.chomp : ARGV.first
+
+# チャンネル名のバリデーション
+if options[:channel].nil? || options[:message].empty?
+  puts "Both message and channel are required."
+  puts opts
+  exit 1
+end
+
+# Slack APIへのリクエストを構築
+uri = URI.parse("https://slack.com/api/chat.postMessage")
+request = Net::HTTP::Post.new(uri)
+request.content_type = "application/json"
+request["Authorization"] = "Bearer #{ENV['SLACK_API_TOKEN']}" # 環境変数からトークンを取得
+request.body = JSON.dump({
+  "channel" => options[:channel],
+  "text" => options[:message]
+})
+
+# HTTPリクエストを送信し、エラーをハンドルする
+begin
+  response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == "https") do |http|
+    http.request(request)
+  end
+
+  # レスポンスのチェック
+  result = JSON.parse(response.body)
+  if response.is_a?(Net::HTTPSuccess) && result["ok"]
+    puts "Message sent successfully."
+  else
+    puts "Error sending message: #{result['error']}"
+    exit 1
+  end
+rescue => e
+  puts "HTTP Request failed: #{e.message}"
+  exit 1
+end
