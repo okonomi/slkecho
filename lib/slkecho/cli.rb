1d2fb5537ed7f2c4a7c49d727cf2d00c66a7d584
okonomi <okonomi@oknm.jp>
2024-01-02 09:48:47 +0900

    extract options struct

--- lib/slkecho/cli.rb
+++ lib/slkecho/cli.rb
@@ -7,22 +7,24 @@ require "optparse"
 
 module Slkecho
   class CLI
+    Options = Struct.new(:channel, :subject, :message, keyword_init: true)
+
     def self.parse_options(argv) # rubocop:disable Metrics/MethodLength
       # コマンドライン引数を解析する
-      options = {}
+      options = Slkecho::CLI::Options.new
       opts = OptionParser.new do |o|
         o.banner = "Usage: echo2slack.rb [options] or echo | echo2slack.rb [options]"
 
         o.on("-c", "--channel CHANNEL", "Slack channel to post the message") do |c|
-          options[:channel] = c
+          options.channel = c
         end
         o.on("-s", "--subject SUBJECT", "Subject of message") do |s|
-          options[:subject] = s
+          options.subject = s
         end
       end
       argv = opts.parse(argv)
 
-      options[:message] = argv.first unless argv.empty?
+      options.message = argv.first unless argv.empty?
 
       options
     end
--- spec/lib/slkecho/cli_spec.rb
+++ spec/lib/slkecho/cli_spec.rb
@@ -7,19 +7,19 @@ RSpec.describe Slkecho::CLI do
     context "when no arguments" do
       let(:argv) { %w[] }
 
-      it { is_expected.to eq({}) }
+      it { is_expected.to eq(Slkecho::CLI::Options.new) }
     end
 
     context "when channel option is given" do
       let(:argv) { %w[-c #general] }
 
-      it { is_expected.to eq(channel: "#general") }
+      it { is_expected.to eq(Slkecho::CLI::Options.new(channel: "#general")) }
     end
 
     context "when channel and subject option is given" do
       let(:argv) { %w[-c #general -s subject] }
 
-      it { is_expected.to eq(channel: "#general", subject: "subject") }
+      it { is_expected.to eq(Slkecho::CLI::Options.new(channel: "#general", subject: "subject")) }
     end
   end
 end
