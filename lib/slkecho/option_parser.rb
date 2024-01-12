751ce1074f3d9c6a362c51ce3e71a364b96207b2
okonomi <okonomi@oknm.jp>
2024-01-12 14:43:59 +0900

    chore: update options description

--- lib/slkecho/option_parser.rb
+++ lib/slkecho/option_parser.rb
@@ -7,11 +7,11 @@ module Slkecho
         o.banner = "Usage: slkecho [options] message"
         o.program_name = "slkecho"
         o.version = Slkecho::VERSION
-        o.on("-c", "--channel CHANNEL", "Slack channel to post the message") { @options.channel = _1 }
-        o.on("-m", "--mention-by-email EMAIL", "Mention to user by email") { @options.mention_by_email = _1 }
-        o.on("--username USERNAME", "Set your bot's user name") { @options.username = _1 }
-        o.on("--icon-url ICON_URL", "URL to an image to use as the icon for this message") { @options.icon_url = _1 }
-        o.on("--icon-emoji ICON_EMOJI", "Emoji to use as the icon for this message") { @options.icon_emoji = _1 }
+        o.on("-c", "--channel CHANNEL", "Slack channel to post message.") { @options.channel = _1 }
+        o.on("-m", "--mention-by-email EMAIL", "Mention to user by email.") { @options.mention_by_email = _1 }
+        o.on("--username USERNAME", "Set user name for message.") { @options.username = _1 }
+        o.on("--icon-url ICON_URL", "Set user icon image for message by URL.") { @options.icon_url = _1 }
+        o.on("--icon-emoji ICON_EMOJI", "Set user image for message by emoji.") { @options.icon_emoji = _1 }
       end
     end
 
