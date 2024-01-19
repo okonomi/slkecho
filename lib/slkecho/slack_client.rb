540d7c5b6c94e5a63974ae028f2b13c836dfb6db
okonomi <okonomi@oknm.jp>
2024-01-19 11:40:22 +0900

    fix: remove unnecessary require and attribute

--- lib/slkecho/slack_client.rb
+++ lib/slkecho/slack_client.rb
@@ -1,9 +1,5 @@
 # frozen_string_literal: true
 
-require "net/http"
-require "uri"
-require "json"
-
 require_relative "slack_request/lookup_user_by_email"
 require_relative "slack_request/post_message"
 
@@ -13,14 +9,6 @@ module Slkecho
 
     def initialize(slack_api_token:)
       @slack_api_token = slack_api_token
-
-      @uri = URI.parse("https://slack.com/api/chat.postMessage")
-      @http = Net::HTTP.new(@uri.host, @uri.port)
-      @http.use_ssl = true
-      @headers = {
-        "Content-Type" => "application/json; charset=utf-8",
-        "Authorization" => "Bearer #{slack_api_token}"
-      }
     end
 
     def lookup_user_by_email(email:)
