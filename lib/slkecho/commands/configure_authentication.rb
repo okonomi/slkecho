d046dea2910996ae59471e9a26df07c512f44a9b
okonomi <okonomi@oknm.jp>
2025-01-04 22:08:00 +0900

    feat: implement client ID and secret input

--- lib/slkecho/commands/configure_authentication.rb
+++ lib/slkecho/commands/configure_authentication.rb
@@ -5,7 +5,20 @@ module Slkecho
     class ConfigureAuthentication
       def execute
         puts "configure slack api authentification"
-        puts "to be implemented"
+
+        client_id, client_secret = gets_oauth2_credentials
+      end
+
+      private
+
+      def gets_oauth2_credentials
+        print "Enter your client ID: "
+        client_id = $stdin.gets.chomp
+
+        print "Enter your client secret: "
+        client_secret = $stdin.gets.chomp
+
+        [client_id, client_secret]
       end
     end
   end
