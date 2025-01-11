ba2a538ac24f9b2a926ced6ed26609f4eb2cf960
okonomi <okonomi@oknm.jp>
2025-01-11 17:51:51 +0900

    feat: retrieve OAuth2 credentials

--- lib/slkecho/command/configure_user_token.rb
+++ lib/slkecho/command/configure_user_token.rb
@@ -5,6 +5,19 @@ module Slkecho
     class ConfigureUserToken
       def execute
         puts "Slkecho configuration"
+        client_id, client_secret = gets_oauth2_credentials
+      end
+
+      private
+
+      def gets_oauth2_credentials
+        print "Enter your Slack App Client ID: "
+        client_id = $stdin.gets.chomp
+
+        print "Enter your Slack App Client Secret: "
+        client_secret = $stdin.gets.chomp
+
+        [client_id, client_secret]
       end
     end
   end
