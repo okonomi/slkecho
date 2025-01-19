633acb3492467c62e98408d9c189128d65122778
okonomi <okonomi@oknm.jp>
2025-01-19 22:54:04 +0900

    refactor: simplify config directory and path methods

--- lib/slkecho/config.rb
+++ lib/slkecho/config.rb
@@ -2,29 +2,27 @@
 
 module Slkecho
   class Config
-    def self.config_dir
-      if ENV.key?("XDG_CONFIG_HOME")
-        Pathname.new(ENV["XDG_CONFIG_HOME"])
-      else
-        Pathname.new(Dir.home).join(".config")
+    class << self
+      def config_dir
+        Pathname.new(ENV["XDG_CONFIG_HOME"] || File.join(Dir.home, ".config"))
       end
-    end
 
-    def self.config_path
-      config_dir.join("slkecho", "token.json")
-    end
+      def config_path
+        config_dir.join("slkecho/token.json")
+      end
 
-    def self.save(data)
-      path = config_path
-      path.dirname.mkpath
-      File.write(path, JSON.pretty_generate(data))
-    end
+      def save(data)
+        path = config_path
+        path.dirname.mkpath
+        File.write(path, JSON.pretty_generate(data))
+      end
 
-    def self.load
-      path = config_path
-      return nil unless path.exist?
+      def load
+        path = config_path
+        return nil unless path.exist?
 
-      JSON.parse(path.read)
+        JSON.parse(path.read)
+      end
     end
   end
 end
